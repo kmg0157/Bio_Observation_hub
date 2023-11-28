03933210fed2a0812dd02b8286c8009c81680e04
kmg0157 <kmg0157@naver.com>
2023-11-29 00:36:46 +0900

    1129_0036

--- join.py
+++ join.py
@@ -2,7 +2,7 @@
 class JoinQuery:
     # 종(Species)와 서식지(Habitat), 관찰기록(ObservationRecord) 정보를 함께 가져오는 쿼리
     query1 = """
-    SELECT DISTINCT ObservationRecord.*, Species.*, Habitat.*
+    SELECT DISTINCT ObservationRecord.*, Species.Name, Species.ConservationStatus, Habitat.Name, Habitat.Location
     FROM ObservationRecord
     JOIN Species ON ObservationRecord.SpeciesID = Species.SpeciesID
     JOIN Habitat ON ObservationRecord.HabitatID = Habitat.HabitatID;
@@ -10,16 +10,16 @@ class JoinQuery:
 
     # 관찰 기록(ObservationRecord)과 연구원(Researcher) 정보를 함께 가져오는 쿼리
     query2 = """
-    SELECT DISTINCT ObservationRecord.*, Researcher.*
+    SELECT DISTINCT ObservationRecord.*, Researcher.ProgramID, Researcher.FirstName, Researcher.LastName
     FROM ObservationRecord
     JOIN Researcher ON ObservationRecord.ResearcherID = Researcher.ResearcherID;
     """
     
     #연구원(Researcher)과 프로그램(ConservationProgram) 정보를 함께 가져오는 쿼리
     query3 = """
-    SELECT DISTINCT Researcher.*, ConservationProgram.*
+    SELECT DISTINCT Researcher.ResearcherID, Researcher.FirstName, Researcher.LastName, ConservationProgram.*
     FROM Researcher
-    JOIN ConservationProgram ON Researcher.ProgramID = ConservationProgram.ProgramID;
+    RIGHT JOIN ConservationProgram ON Researcher.ProgramID = ConservationProgram.ProgramID;
     """
 
     
